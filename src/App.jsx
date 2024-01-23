0d50c24a5bbaf84e75c550966dbea1fafbc73787
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-23 17:19:34 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sanober Sarfaraz Ahmed",
+  title: "ML and Full Stack Developer",
+  email: "sanoberahmed494@gmail.com",
+  gitHub: "Sanober494",
+  instagram: "sanober_494",
+  linkedIn: "https://www.linkedin.com/in/sanober-sarfaraz-ahmed/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
